22b365de94327b2532e451f536cdfc65f652032c
codeynoor <68320326+codeynoor@users.noreply.github.com>
2023-12-24 01:37:29 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+import asyncio
+import os
+
+prefix = 'YOUR_PREFIX' # Enter a prefix
+client = commands.Bot(command_prefix=prefix, intents=discord.Intents.all())
+
+# Remove the built-in help and ping commands
+client.remove_command('help') 
+client.remove_command('ping')
+
+@client.event
+async def on_ready():
+  print(f'\nLogged in as {client.user}\nJoined servers: {len(client.guilds)}')
+
+# Error handler
+@client.event
+async def on_command_error(ctx, error):
+  if isinstance(error, commands.errors.CommandNotFound):
+    await ctx.send("The specified command doesn't exist")
+  else:
+    await ctx.send("An error occured")
+    print(error)
+
+# Example command
+@client.command(name="ping", description="Ping command")
+async def ping(ctx):
+  await ctx.send(f'{round(client.latency * 1000)} ms')
+
+# Start your bot 
+client.run('YOUR_TOKEN') 
