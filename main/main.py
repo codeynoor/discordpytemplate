ae8d79d3e9e85c98e5851713f01bd52882f01f0c
codeynoor <68320326+codeynoor@users.noreply.github.com>
2023-12-24 02:21:28 +0600

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 import asyncio
-import os
 
 prefix = 'YOUR_PREFIX' # Enter a prefix
 client = commands.Bot(command_prefix=prefix, intents=discord.Intents.all())
